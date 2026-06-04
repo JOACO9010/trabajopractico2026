529ec71215c5e8d75422d4a4888a77a6e26ae8d3
alumno <52211252@ivt.edu.ar>
2026-06-04 09:07:57 -0300

    04/06

--- tp5/app.js
+++ tp5/app.js
@@ -1,12 +1,14 @@
-let suma = 10
-let resta = 20
-let multiplicacion = 2
-let division = 8
-alert (10+20)
-alert (20-10)
-alert(2*2)
-alert(10/2)
-console.log(10+20);
-console.log(20-10)
-console.log(2*2)
-console.log(10/2)
+let num1=
+alert("el resultado de suma es: +suma")
+//suma
+let resta = 20-10
+alert ("el resultado de resta es: + resta" )
+//resta
+let division=10/2
+alert("el resultado de resta es: + division")
+//division
+let multipliclacion= 10*2
+alert("el resultado de resta es: + multiplicacion")
+//multiplicacion
+
+
