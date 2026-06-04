08ebec21b61f22c5c0b36505c10227b76e8a7c52
alumno <52211252@ivt.edu.ar>
2026-06-04 08:07:11 -0300

    tp5

--- tp5/app.js
+++ tp5/app.js
@@ -1,8 +1,12 @@
-let numero = 25
-let numero = 23
-let resultado
-
-resultado = numero1 + numero2
-
-console.log(resultado);
-alert(resultado)
+let suma = 10
+let resta = 20
+let multiplicacion = 2
+let division = 8
+alert (10+20)
+alert (20-10)
+alert(2*2)
+alert(10/2)
+console.log(10+20);
+console.log(20-10)
+console.log(2*2)
+console.log(10/2)
