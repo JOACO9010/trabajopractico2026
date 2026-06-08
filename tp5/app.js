8f862eb367ad83be4dc09a48541968f2f37892c2
JOACO9010 <52211252@ivt.edu.ar>
2026-06-08 11:21:30 -0300

    app.js camios

--- tp5/app.js
+++ tp5/app.js
@@ -1,14 +1,40 @@
-let num1=
-alert("el resultado de suma es: +suma")
-//suma
-let resta = 20-10
-alert ("el resultado de resta es: + resta" )
-//resta
-let division=10/2
-alert("el resultado de resta es: + division")
-//division
-let multipliclacion= 10*2
-alert("el resultado de resta es: + multiplicacion")
-//multiplicacion
+let suma
+let resta 
+let multiplicacion 
+let division 
+suma = (20+20)
+resta = (20-10)
+multiplicacion = (2*2)
+division = (10/2)
+alert (suma)
+alert (resta);
+console.log (multiplicacion)
+console.log (division)
+alert ("el resultado de la suma es:" + suma)
+alert ("el resultado de la resta es:" + resta)
+console.log ("el resultado de la multiplicacion es:" + multiplicacion)
+console.log ("el resultado de la division es:" + division)
+let nombreAlumno = "juan"
+let apellidoAlumno = "perez"
+let edadAlumno = 23
+console.log ("El alumno: " + nombreAlumno + " " + apellidoAlumno + " tiene: " + edadAlumno + " años.")
+let precio1 = 123
+let precio2 = 45
+let precio3 = 80
+let total
+total (precio1+precio2+precio3)
+console.log ("el total a pagar es de:" +total)
+let comidafavorita = "panchos"
+let colorfavorito = "azul"
+let numerofavorito = 7
+console.log ("Mi comida favorita son los "+comidafavorita+", mi color favorito es el "+colorfavorito+" y mi número favorito es el "+numerofavorito)
+let pais
+pais = "argentina"
+console.log
+
+
+
+
+
 
 
