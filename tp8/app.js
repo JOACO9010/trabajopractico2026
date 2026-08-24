b8046f68d1099a393ffc3325f30fbdee2bd5ffed
JOACO9010 <52211252@ivt.edu.ar>
2026-08-24 11:21:48 -0300

    24 de agosto

---
+++ tp8/app.js
@@ -0,0 +1,88 @@
+let edad = 20
+let texto = document.querySelector("#texto")
+let btn1 = document.querySelector("#btn1")
+let input1 = document.querySelector("#input1")
+
+btn1.onclick = function() {
+    if (edad >= 18)  {
+        texto.textContent = input1.value 
+    }
+    else {
+        texto.textContent = "Eres menor de edad"
+    }
+}
+let Ej2 = document.querySelector('#Ej2')
+let botonEj2 = document.querySelector('#botonEj2')
+let input2 = document.querySelector("#input2")
+let nombreUsuario = 'Julian'
+botonEj2.onclick = function(){
+    if (nombreUsuario = 'Julian'){
+        Ej2.textContent = 'Bienvenido Julian, ¿como estas?'
+    } else {
+        Ej2.textContent = 'Bienvenido usuario'
+    }
+}
+let Ej3 = document.querySelector('#Ej3')
+let botonEj3 = document.querySelector('#botonEj3')
+let input3 = document.querySelector('#input3')
+let nombreUsuario2 = 'Jorge'
+botonEj3.onclick = function(){
+    if (nombreUsuario2 == 'Jorge'||nombreUsuario2 == 'Roberto'){
+        Ej3.textContent = 'Bienvenido + nombreUsuario2 + , ¿como estas?' 
+    } else {
+        Ej3.textContent = 'Bienvenido + nombreUsuario2'
+    }
+}
+let Ej4 = document.querySelector('#Ej4')
+let botonEj4 = document.querySelector('#botonEj4')
+let numero = 5
+botonEj4.onclick = function(){
+    if (numero > 0) {
+        Ej4.textContent = 'el numero es positivo'
+    } else if (numero < 0) {
+        Ej4.textContent = 'el numero es negativo'
+    } else {
+        Ej4.textContent = 'el numero es cero'
+    }
+}
+let Ej5 = document.querySelector('#Ej5')
+let botonEj5 = document.querySelector('#botonEj5')
+let edad2 = 14
+botonEj5.onclick = function(){
+    if (edad2>= 6 && edad2 <= 11) {
+        Ej5.textContent = 'niño'
+    } else if (edad2>= 19 && edad2 <= 18) {
+        Ej5.textContent = 'adolescente'
+    } else if (edad2>= 19 && edad2 <= 26) {
+        Ej5.textContent = 'joven'
+    } else if (edad2>= 27 && edad2 <= 59) {
+        Ej5.textContent = 'adulto'
+    } else if (edad2>= 60) {
+        Ej5.textContent = 'anciano'
+    } else {
+        Ej5.textContent = 'edad no valida'
+    }
+}
+let Ej6 = document.querySelector("#Ej6")
+let botonEj6 = document.querySelector('#botonEj6')
+let dia = 'Sabado'
+botonEj6.onclick = function(){
+    if (dia = 'lunes, martes, miercoles, jueves o viernes') {
+        Ej6.textContent = 'es un dia laborable'
+    } else if (dia = 'sabado o domingo') {
+        Ej6.textContent = 'es fin de semana'
+    }
+}
+let Ej7 = document.querySelector('#Ej7')
+let botonEj7 = document.querySelector('#botonEj7')
+let contraseña = 'secreto'
+botonEj7.onclick = function(){
+    if (contraseña = 'secreto') {
+        Ej7.textContent = 'acceso concedido'
+    } else {
+        Ej7.textContent = 'acceso denegado'
+    }
+}
+
+
+
