4fdc077d024c9e74e2b87bb2f35d44a679e4ba18
JOACO9010 <52211252@ivt.edu.ar>
2026-08-31 10:59:56 -0300

    31 de agosto

--- tp8/app.js
+++ tp8/app.js
@@ -1,83 +1,91 @@
-let edad = 20
-let texto = document.querySelector("#texto")
-let btn1 = document.querySelector("#btn1")
+let texto = document.querySelector("#Ej1")
+let botonEj1 = document.querySelector("#btn1")
 let input1 = document.querySelector("#input1")
 
-btn1.onclick = function() {
-    if (edad >= 18)  {
-        texto.textContent = input1.value 
+botonEj1.onclick = function() {
+    if (input1.value >= 18)  {
+        texto.textContent = "Eres mayor de edad"
     }
     else {
         texto.textContent = "Eres menor de edad"
     }
 }
+
 let Ej2 = document.querySelector('#Ej2')
-let botonEj2 = document.querySelector('#botonEj2')
+let botonEj2 = document.querySelector('#btn2')
 let input2 = document.querySelector("#input2")
-let nombreUsuario = 'Julian'
+
 botonEj2.onclick = function(){
-    if (nombreUsuario = 'Julian'){
-        Ej2.textContent = 'Bienvenido Julian, ¿como estas?'
+    if (input2.value == 'Julian'){
+        Ej2.textContent = "Bienvenido Julian, ¿como estas?"
     } else {
-        Ej2.textContent = 'Bienvenido usuario'
+        Ej2.textContent = "Bienvenido usuario"
     }
 }
+
 let Ej3 = document.querySelector('#Ej3')
-let botonEj3 = document.querySelector('#botonEj3')
+let botonEj3 = document.querySelector('#btn3')
 let input3 = document.querySelector('#input3')
-let nombreUsuario2 = 'Jorge'
+
 botonEj3.onclick = function(){
-    if (nombreUsuario2 == 'Jorge'||nombreUsuario2 == 'Roberto'){
-        Ej3.textContent = 'Bienvenido + nombreUsuario2 + , ¿como estas?' 
+    if (input3.value == 'Jorge'|| input3.value == 'Roberto'){
+        Ej3.textContent = 'Bienvenido ' + input3.value + ', ¿como estas?' 
     } else {
-        Ej3.textContent = 'Bienvenido + nombreUsuario2'
+        Ej3.textContent = 'Bienvenido ' + input3.value
     }
 }
+
 let Ej4 = document.querySelector('#Ej4')
-let botonEj4 = document.querySelector('#botonEj4')
-let numero = 5
+let botonEj4 = document.querySelector('#btn4')
+let input4 = document.querySelector('#input4')
+
 botonEj4.onclick = function(){
-    if (numero > 0) {
+    if (input4.value > 0) {
         Ej4.textContent = 'el numero es positivo'
-    } else if (numero < 0) {
+    } else if (input4.value < 0) {
         Ej4.textContent = 'el numero es negativo'
     } else {
         Ej4.textContent = 'el numero es cero'
     }
 }
+
 let Ej5 = document.querySelector('#Ej5')
-let botonEj5 = document.querySelector('#botonEj5')
-let edad2 = 14
+let botonEj5 = document.querySelector('#btn5')
+let input5 = document.querySelector('#input5')
+
 botonEj5.onclick = function(){
-    if (edad2>= 6 && edad2 <= 11) {
+    if (input5.value >= 6 && input5.value <= 11) {
         Ej5.textContent = 'niño'
-    } else if (edad2>= 19 && edad2 <= 18) {
+    } else if (input5.value>= 12 && input5.value <= 18) {
         Ej5.textContent = 'adolescente'
-    } else if (edad2>= 19 && edad2 <= 26) {
+    } else if (input5.value>= 19 && input5.value <= 26) {
         Ej5.textContent = 'joven'
-    } else if (edad2>= 27 && edad2 <= 59) {
+    } else if (input5.value>= 27 && input5.value <= 59) {
         Ej5.textContent = 'adulto'
-    } else if (edad2>= 60) {
+    } else if (input5.value>= 60) {
         Ej5.textContent = 'anciano'
     } else {
         Ej5.textContent = 'edad no valida'
     }
 }
 let Ej6 = document.querySelector("#Ej6")
-let botonEj6 = document.querySelector('#botonEj6')
-let dia = 'Sabado'
+let botonEj6 = document.querySelector('#btn6')
+let input6 = document.querySelector('#input6')
+
 botonEj6.onclick = function(){
-    if (dia = 'lunes, martes, miercoles, jueves o viernes') {
+    if (input6.value == 'lunes' || input6.value == 'martes' || input6.value == 'miercoles' || input6.value == 'jueves' || input6.value == 'viernes') {
         Ej6.textContent = 'es un dia laborable'
-    } else if (dia = 'sabado o domingo') {
+    } else if (input6.value == 'sabado' || input6.value == 'domingo') {
         Ej6.textContent = 'es fin de semana'
     }
 }
+
 let Ej7 = document.querySelector('#Ej7')
-let botonEj7 = document.querySelector('#botonEj7')
-let contraseña = 'secreto'
+let botonEj7 = document.querySelector('#btn7')
+let input7 = document.querySelector('#input7')
+
 botonEj7.onclick = function(){
-    if (contraseña = 'secreto') {
+    if (input7.value == 'secreto') {
         Ej7.textContent = 'acceso concedido'
     } else {
         Ej7.textContent = 'acceso denegado'
